88b77a151d43d5f89a1e78cc00e8181f8894f02b
Linus Heckemann <git@sphalerite.org>
2016-11-11 17:54:08 +0000

    Lots of chrome!

--- prettyxmpp.js
+++ prettyxmpp.js
@@ -6,6 +6,10 @@ $(function() {
     views.login = $('#loginView');
     views.progress = $('#progressView');
     views.connected = $('#connectedView');
+    views.chats = {};
+    views.chatListItems = {
+        ACTIVE: $('<div>') // Dummy
+    };
 
     var loginButton = $('#loginButton');
     loginButton.val('Log in');
@@ -38,7 +42,8 @@ function switchToView(switchTo) {
 function validateLoginForm() {
     var params = {
         jid: new XMPP.JID($('#loginJid').val()),
-        password: $('#loginPw').val()
+        password: $('#loginPw').val(),
+        useStreamManagement: true
     };
 
     if ($('#loginSave').prop('checked')) {
@@ -55,21 +60,61 @@ function connect(params) {
 
     client.on('session:started', function() {
         client.sendPresence();
+        client.enableCarbons();
+        client.getRoster().then(updateRoster, rosterFailed);
+        
         switchToView(views.connected);
     });
 
-    client.on('chat', function(msg) {
-        var messageEl = $('<div/>'),
-            authorEl = $('<b/>'),
-            bodyEl = $('<span/>');
-        messageEl.addClass('message');
-        authorEl.addClass('author');
-        bodyEl.addClass('body');
-        authorEl.text(msg.from);
-        bodyEl.text(msg.body);
-        messageEl.append(authorEl);
-        messageEl.append(bodyEl);
-        $('#chatBox').append(messageEl);
-    });
+    client.on('chat', onMessage);
+
     client.connect();
+}
+
+function onMessage(msg) {
+    var messageEl = $('<div/>'),
+        authorEl = $('<b/>'),
+        bodyEl = $('<span/>');
+    messageEl.addClass('message');
+    authorEl.addClass('author');
+    bodyEl.addClass('body');
+    authorEl.text(msg.from);
+    bodyEl.text(msg.body);
+    messageEl.append(authorEl);
+    messageEl.append(bodyEl);
+    $('#chatBox > *').append(messageEl);
+}
+
+function updateRoster(reply) {
+    var rosterView = $('#roster');
+    rosterView.empty();
+    for (contact of reply.roster.items) {
+        contactItem = $('<a>');
+        contactItem.addClass('contact');
+        contactItem.text(contact.jid.bare);
+        contactItem.click(e => switchToChat(e.toElement.innerText));
+        views.chatListItems[contact.jid.bare] = contactItem;
+        rosterView.append(contactItem);
+    }
+}
+
+function switchToChat(jid) {
+    views.chatListItems.ACTIVE.removeClass("active");
+    views.chatListItems.ACTIVE = views.chatListItems[jid];
+    views.chatListItems[jid].addClass("active");
+    var view = views.chats[jid];
+    if (!view) {
+        view = $('<div>');
+        view.addClass('chat');
+        view.text('hello');
+        views.chats[jid] = view;
+    }
+    $('#chatBox').empty();
+    $('#chatBox').append(view);
+}
+
+function rosterFailed() {
+    var roster = $('#roster');
+    roster.empty();
+    roster.append($('<div class="rosterError">Could not get roster</div>'));
 }
